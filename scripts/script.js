3d3421a4c1a234d856824b6756b788b1eafb1e6e
Sunray777 <alpzm7@gmail.com>
2020-12-30 16:53:48 +0300

    Доработки по меню

--- scripts/script.js
+++ scripts/script.js
@@ -24,8 +24,19 @@ if(isMobile.any())
 {
     body.classList.add('touch');
     let arrow = document.querySelectorAll('.arrow');
-    }
-    else{
+    for(i=0; i<arrow.length; i++)
+    {
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parent');
+    arrow[i].addEventListener('click', function(){
+    subMenu.classList.toggle('open');
+    thisArrow.classList.toggle('active');
+});    
+}
+}else{
         body.classList.add('mouse');
     }
 
